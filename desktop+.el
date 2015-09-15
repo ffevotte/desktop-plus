4051f4e3b885bd8a42659ba408829a3e04b202ab
François Févotte <fevotte@gmail.com>
2015-09-15 09:12:58 +0200

    Stop using `names`

--- desktop+.el
+++ desktop+.el
@@ -4,7 +4,7 @@
 ;; Author:  François Févotte <fevotte@gmail.com>
 ;; URL: https://github.com/ffevotte/desktop-plus
 ;; Version: 0.1.1
-;; Package-Requires: ((emacs "24.4") (dash "2.11.0") (f "0.17.2") (names "0.5.4"))
+;; Package-Requires: ((emacs "24.4") (dash "2.11.0") (f "0.17.2"))
 
 ;; This file is NOT part of Emacs
 
@@ -53,24 +53,19 @@
 
 ;;; Code:
 (eval-when-compile
-  (require 'dash)
-  (require 'names))
+  (require 'dash))
 
 (require 'desktop)
 (require 'f)
-
-
-;;;###autoload
-(define-namespace desktop+-
 
 ;; * Named sessions
 
 ;; ** Customizable options
 
-(defvar base-dir "~/.emacs.d/desktops/"
+(defvar desktop+-base-dir "~/.emacs.d/desktops/"
   "Base directory for desktop files.")
 
-(defvar frame-title-function 'desktop+--frame-title
+(defvar desktop+-frame-title-function 'desktop+--frame-title
   "Function returning the frame title when a desktop session is loaded.
 
 This function must accept the desktop name as a string argument
@@ -79,8 +74,8 @@ and return a frame title format suitable for setting
 
 ;; ** Entry points
 
-:autoload
-(defun create (name)
+;;;###autoload
+(defun desktop+-create (name)
   "Create a new session, identified by a name.
 The session is created in a subdirectory of `desktop+-base-dir'.
 It can afterwards be reloaded using `desktop+-load'.
@@ -89,22 +84,22 @@ As a special case, if NAME is left blank, the session is
 automatically named after the current working directory."
   (interactive "MDesktop name: ")
   (desktop-kill)
-  (setq desktop-dirname (-dirname name))
+  (setq desktop-dirname (desktop+--dirname name))
   (make-directory desktop-dirname 'parents)
   (desktop-save desktop-dirname)
-  (-set-frame-title)
+  (desktop+--set-frame-title)
   (desktop-save-mode 1))
 
-:autoload
-(defun create-auto ()
+;;;###autoload
+(defun desktop+-create-auto ()
   "Create a new session, identified by the current working directory.
 The session is created in a subdirectory of `desktop+-base-dir'.
 It can afterwards be reloaded using `desktop+-load'."
   (interactive)
-  (create ""))
+  (desktop+-create ""))
 
-:autoload
-(defun load (name)
+;;;###autoload
+(defun desktop+-load (name)
   "Load a session previously created using `desktop+-create'.
 NAME is the name which was given at session creation.  When
 called interactively, it is asked in the minibuffer with
@@ -115,32 +110,34 @@ automatically named after the current working directory."
   (interactive
    (list
     (completing-read "Desktop name: "
-                     (remove "." (remove ".." (directory-files base-dir))))))
-  (desktop-change-dir (-dirname name))
-  (-set-frame-title)
+                     (remove "."
+                             (remove ".."
+                                     (directory-files desktop+-base-dir))))))
+  (desktop-change-dir (desktop+--dirname name))
+  (desktop+--set-frame-title)
   (desktop-save-mode 1))
 
-:autoload
-(defun load-auto ()
+;;;###autoload
+(defun desktop+-load-auto ()
   "Load a session previously created using `desktop+-create-auto'.
 The session is identified by the current working directory."
   (interactive)
-  (load ""))
+  (desktop+-load ""))
 
 ;; ** Inner workings
 
-(defun -dirname (name)
+(defun desktop+--dirname (name)
   "Path to the desktop identified by NAME.
 As a special case, if NAME is blank, the directory is identified
 by the current working directory.
 
 This path is located under `desktop+-base-dir'."
-  (concat base-dir
+  (concat desktop+-base-dir
           (if (string= "" name)
               (replace-regexp-in-string "/" "-" (f-canonical default-directory))
             name)))
 
-(defun -frame-title (desktop-name)
+(defun desktop+--frame-title (desktop-name)
   "Default frame title function for sessions.
 
 Returns the following frame title format:
@@ -148,10 +145,10 @@ Returns the following frame title format:
   (list (concat "%b - Emacs [" desktop-name "]")))
 
 
-(defun -set-frame-title ()
+(defun desktop+--set-frame-title ()
   "Set the frame title to show the currently active session."
   (setq frame-title-format
-        (funcall frame-title-function
+        (funcall desktop+-frame-title-function
                  (file-name-nondirectory (directory-file-name desktop-dirname)))))
 
 
@@ -167,26 +164,26 @@ Returns the following frame title format:
 
 ;; ** Entry point
 
-:autoload
-(defun -advice--desktop-save (&rest args)
+;;;###autoload
+(defun desktop+--advice--desktop-save (&rest args)
   "Also save special buffers."
-  (-buffers-save))
-:autoload
-(advice-add 'desktop-save :before #'-advice--desktop-save)
+  (desktop+--buffers-save))
+;;;###autoload
+(advice-add 'desktop-save :before #'desktop+--advice--desktop-save)
 
-:autoload
-(defun -advice--desktop-restore-frameset (&rest args)
+;;;###autoload
+(defun desktop+--advice--desktop-restore-frameset (&rest args)
   "Restore special buffers."
-  (-buffers-load))
-:autoload
-(advice-add 'desktop-restore-frameset :before #'-advice--desktop-restore-frameset)
+  (desktop+--buffers-load))
+;;;###autoload
+(advice-add 'desktop-restore-frameset :before #'desktop+--advice--desktop-restore-frameset)
 
 ;; ** Mode-specific handlers for special buffers
 
-(defvar -special-buffer-handlers-alist nil
+(defvar desktop+--special-buffer-handlers-alist nil
   "Alist of handlers for special buffers.")
 
-(defun add-handler (name pred save-fn load-fn &optional activate)
+(defun desktop+-add-handler (name pred save-fn load-fn &optional activate)
   "Add handlers for special buffers.
 
 NAME is a symbol identifying the handler for later activation or
@@ -217,68 +214,65 @@ modes in variable `desktop+-special-buffer-handlers'."
   (when activate
     (add-to-list 'desktop+-special-buffer-handlers name))
   (push (list name pred save-fn load-fn)
-        -special-buffer-handlers-alist))
+        desktop+--special-buffer-handlers-alist))
 
 ;; *** Terminals
 
-(add-handler
- 'term-mode
- nil
+(desktop+-add-handler 'term-mode
+  nil
 
- (lambda ()
-   "Return relevant parameters for saving a terminal buffer."
-   (list :dir     default-directory
-         :command (car (last (process-command
-                              (get-buffer-process (current-buffer)))))))
+  (lambda ()
+    "Return relevant parameters for saving a terminal buffer."
+    (list :dir     default-directory
+          :command (car (last (process-command
+                               (get-buffer-process (current-buffer)))))))
 
- (lambda (name &rest args)
-   "Restore a terminal buffer from saved parameters."
-   (when (null (get-buffer name))
-     (let ((default-directory (plist-get args :dir)))
-       (with-current-buffer (term (plist-get args :command))
-         (rename-buffer name))))))
+  (lambda (name &rest args)
+    "Restore a terminal buffer from saved parameters."
+    (when (null (get-buffer name))
+      (let ((default-directory (plist-get args :dir)))
+        (with-current-buffer (term (plist-get args :command))
+          (rename-buffer name))))))
 
 ;; *** Compilation buffers
 
 (eval-when-compile
   (require 'compile))
 
-(add-handler
- 'compilation-mode
- nil
+(desktop+-add-handler 'compilation-mode
+  nil
 
- (lambda ()
-   "Return relevant parameters for saving a compilation buffer."
-   (list :command `(quote ,compilation-arguments)
-         :dir     compilation-directory))
+  (lambda ()
+    "Return relevant parameters for saving a compilation buffer."
+    (list :command `(quote ,compilation-arguments)
+          :dir     compilation-directory))
 
- (lambda (name &rest args)
-   "Restore a compilation buffer from saved parameters."
-   (with-current-buffer (get-buffer-create name)
-     (compilation-mode)
-     (set (make-local-variable 'compilation-arguments) (plist-get args :command))
-     (set (make-local-variable 'compilation-directory) (plist-get args :dir)))))
+  (lambda (name &rest args)
+    "Restore a compilation buffer from saved parameters."
+    (with-current-buffer (get-buffer-create name)
+      (compilation-mode)
+      (set (make-local-variable 'compilation-arguments) (plist-get args :command))
+      (set (make-local-variable 'compilation-directory) (plist-get args :dir)))))
 
 ;; *** Clones (indirect buffers)
 
-(add-handler
- 'indirect-buffer
- #'buffer-base-buffer
+(desktop+-add-handler 'indirect-buffer
+  #'buffer-base-buffer
 
- (lambda ()
-   `(:base ,(buffer-name (buffer-base-buffer))))
+  (lambda ()
+    `(:base ,(buffer-name (buffer-base-buffer))))
 
- (lambda (name &rest args)
-   (with-current-buffer (get-buffer (plist-get args :base))
-     (clone-indirect-buffer name nil))))
+  (lambda (name &rest args)
+    (with-current-buffer (get-buffer (plist-get args :base))
+      (clone-indirect-buffer name nil))))
 
 ;; ** Inner workings
 
-(defun -buffers-file ()
+(defun desktop+--buffers-file ()
   "Name of the file where special buffers configuration will be saved."
   (concat desktop-dirname "/.emacs-buffers"))
 
-(defun -create-buffer (key name &rest args)
+(defun desktop+--create-buffer (key name &rest args)
   "Recreate a special buffer from saved parameters.
 
 KEY identifies the special buffer type, as registered in
@@ -289,13 +283,13 @@ NAME is the name of the buffer.
 ARGS is the relevant buffer parameters, as determined by the
 registered save handler.  These parameters will be restored by
 calling the load handler."
-  (let ((handler (assq key -special-buffer-handlers-alist)))
+  (let ((handler (assq key desktop+--special-buffer-handlers-alist)))
     (when handler
       (apply (nth 3 handler) name args))))
 
-(defun -buffers-save ()
+(defun desktop+--buffers-save ()
   "Persistently save special buffers.
-Information is kept in the file pointed to by `-buffers-file'."
+Information is kept in the file pointed to by `desktop+--buffers-file'."
   (with-temp-buffer
     (mapc (lambda (b)
             (let ((data
@@ -304,7 +298,7 @@ Information is kept in the file pointed to by `-buffers-file'."
                             (--first
                              (and (memq (nth 0 it) special-buffer-handlers)
                                   (funcall (nth 1 it)))
-                             -special-buffer-handlers-alist)))
+                             desktop+--special-buffer-handlers-alist)))
                        (when handler
                          (append `(desktop+--create-buffer
                                    (quote ,(nth 0 handler))
@@ -313,14 +307,14 @@ Information is kept in the file pointed to by `-buffers-file'."
               (if data
                   (pp data (current-buffer)))))
           (buffer-list))
-    (write-region nil nil (-buffers-file))))
+    (write-region nil nil (desktop+--buffers-file))))
 
-(defun -buffers-load ()
+(defun desktop+--buffers-load ()
   "Load special buffers from the persistent session file.
 Information is kept in the file pointed to by
-`desktop+--buffers-file'."
-  (when (file-exists-p (-buffers-file))
-    (load-file (-buffers-file)))))
+`desktop+-desktop+--buffers-file'."
+  (when (file-exists-p (desktop+--buffers-file))
+    (load-file (desktop+--buffers-file))))
 
 (provide 'desktop+)
 
